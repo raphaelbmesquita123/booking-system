9974938ac35fa31c04602028f764a5c36d324d9a
raphaelbmesquita123 <raphaelbmesquita@gmail.com>
2021-10-20 13:58:09 +0100

    import React

--- src/components/bookingForm/index.tsx
+++ src/components/bookingForm/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { toast } from "react-toastify";
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
+import React from 'react'
 import type { NextPage } from "next";
 import Head from "next/head";
-import Img from "next/image";
 
 //components
 import { BookingForm } from "../components/bookingForm";
