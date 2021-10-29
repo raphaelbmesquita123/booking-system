33593ac530f05157f219586b4a40364002296be0
raphaelbmesquita123 <raphaelbmesquita@gmail.com>
2021-10-29 15:38:01 +0100

    add module babel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,25 @@
-import React from "react";
-import NextDocument from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends NextDocument {
-  static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
 
-      const initialProps = await NextDocument.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
@@ -22,10 +27,22 @@ export default class MyDocument extends NextDocument {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      };
+        )
+      }
     } finally {
-      sheet.seal();
+      sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
